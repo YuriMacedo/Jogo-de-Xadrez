1f95b896a69f9271e9158576aa1f4141d59929bd
YuriMacedo <yuri_paulino@hotmail.com>
2022-08-02 14:15:43 -0300

    Classe Posicao Criada

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Xadrez_Console.tabuleiro;
 
 namespace Xadrez_Console
 {
@@ -6,7 +7,11 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: "+P);
         }
     }
 }
