fdd7451874dba92e2ac723d95f5b8c9b8eb18195
YuriMacedo <yuri_paulino@hotmail.com>
2022-08-02 14:36:22 -0300

    Começo da criação do tabuleiro

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Xadrez_Console.tabuleiro;
+using tabuleiro;
 
 namespace Xadrez_Console
 {
@@ -7,11 +7,8 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            Posicao P;
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
-            P = new Posicao(3, 4);
-
-            Console.WriteLine("Posição: "+P);
         }
     }
 }
---
+++ Xadrez_Console/tabuleiro/Cor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    public enum Cor
+    {
+        
+
+            Branca,
+            Preta,
+            Amarelo,
+            Azul,
+            Vermelho,
+            Verde,
+            Laranja
+    }
+}
